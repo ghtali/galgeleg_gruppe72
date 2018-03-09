85641a25d208887a89290d3953e9f7580a9c71c1
Ali.Gh <ght.ali@hotmail.com>
2018-03-09 11:38:45 +0100

    Test #1 spring boot works

--- src/test/java/com/galgeleg/MustacheApplicationTests.java
+++ src/test/java/com/galgeleg/MustacheApplicationTests.java
@@ -32,7 +32,7 @@ public class MustacheApplicationTests {
 
         ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(entity.getBody()).contains("Hello Mkyong");
+        assertThat(entity.getBody()).contains("Hello Folkens");
 
     }
 
